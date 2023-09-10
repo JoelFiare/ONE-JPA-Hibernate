8b4ad3a9c5db34ce20634cbb7245a6bd344acfcd
Joel Fiaré <joelfiare@gmail.com>
2023-09-10 12:46:38 -0300

    Changes

--- src/main/java/com/latam/alura/tienda/dao/ProductoDao.java
+++ src/main/java/com/latam/alura/tienda/dao/ProductoDao.java
@@ -1,10 +1,12 @@
 package com.latam.alura.tienda.dao;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import com.latam.alura.tienda.modelo.Producto;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 public class ProductoDao {
@@ -40,5 +42,31 @@ public class ProductoDao {
 
 	public BigDecimal consultarPrecioPorNombreDeProducto (String nombre) {
 		return em.createNamedQuery("Producto.consultaDePrecio",BigDecimal.class).setParameter("nombre", nombre).getSingleResult();
+	}
+
+	public List<Producto> consultarPorParametros(String nombre, BigDecimal precio, LocalDate fecha) {
+		StringBuilder jpql = new StringBuilder("select p from Producto p where 1=1 ");
+		if(nombre != null && !nombre.trim().isEmpty()) {
+			jpql.append("and p.nombre=:nombre ");
+		}
+		if(precio != null && !precio.equals(new BigDecimal(0))) {
+			jpql.append("and p.precio=:precio ");
+		}
+		if(fecha != null) {
+			jpql.append("and p.fechaDeRegistro=:fecha ");
+		}
+
+		TypedQuery<Producto> query = em.createQuery(jpql.toString(), Producto.class);
+
+		if(nombre != null && !nombre.trim().isEmpty()) {
+			query.setParameter("nombre", nombre);
+		}
+		if(precio != null && !precio.equals(new BigDecimal(0))) {
+			query.setParameter("precio", precio);
+		}
+		if(fecha != null) {
+			query.setParameter("fechaDeRegistro", fecha);
+		}
+		return query.getResultList();
 	}
 }
